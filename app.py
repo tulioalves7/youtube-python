fa0f63ddd8c73c4bf369688ebe7620f7b926ab25
Tulio Braga <81985159+tulioalves7@users.noreply.github.com>
2024-09-06 11:59:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ from moviepy.editor import *
 
 app = Flask(__name__)
 
-# Rota principal da interface
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Rota para processar o download
 @app.route('/download', methods=['POST'])
 def download_video():
     url = request.form['url']
@@ -24,19 +22,16 @@ def download_video():
             stream.download(output_path=download_path)
             file_path = os.path.join(download_path, stream.default_filename)
         else:
-            # Baixar o vídeo na melhor qualidade e depois converter para mp3
             stream = yt.streams.filter(only_audio=True).first()
             stream.download(output_path=download_path)
             file_path = os.path.join(download_path, stream.default_filename)
 
-            # Convertendo para mp3
             mp4_path = file_path
             mp3_path = mp4_path.replace('.mp4', '.mp3')
             video = AudioFileClip(mp4_path)
             video.write_audiofile(mp3_path)
             video.close()
 
-            # Remover o arquivo de vídeo mp4
             os.remove(mp4_path)
 
             file_path = mp3_path
